646c7280a7632da45dc18d8ffe92f32658c0f7c3
Purple Circuits <purplecircuits06@gmail.com>
2021-08-28 17:53:25 -0500

    Create Trigmecanum.java

---
+++ teamcode/Trigmecanum.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class Trigmecanum {
+
+    public DcMotor motorBackLeft;
+    public DcMotor motorBackRight;
+    public DcMotor motorFrontLeft;
+    public DcMotor motorFrontRight;
+
+    public double motor0Scaled;
+    public double motor1Scaled;
+    public double motor2Scaled;
+    public double motor3Scaled;
+    public double slowdown = 1.0;
+
+    private ElapsedTime runtime = new ElapsedTime() ;
+
+    HardwareMap hwMap;
+
+    BNO055IMU imu;
+
+    Orientation angles;
+    Acceleration gravity;
+
+    //how many shaft rotations to wheel rotations
+    //TODO what is the shaft to wheel ratio? ticks for wheel (383.6 for 40 motor)
+    double shaftToWheelRatio = 1;
+    double ticksPerShaftRotation =
+    double ticksPerWheelRotation = shaftToWheelRatio * ticksPerShaftRotation;
+    double lrOfset;
+    double fbOfset;
+    //TODO measure robot
+    //robot dimensions
+    double robotDiameter = 25.46; //TODO this is if the robot is exactly 18*18 may need to change
+    double wheelDiameter = 3.75; //both in inches
+    double wheelCircumference = 3.14 * wheelDiameter;
+
+    public void init(HardwareMap ahwMap)  {
+        hwMap = ahwMap;
+
+        motorFrontLeft = hwMap.get(DcMotor.class, "motorFrontLeft");
+        motorFrontRight = hwMap.get(DcMotor.class, "motorFrontRight");
+        motorBackRight = hwMap.get(DcMotor.class, "motorBackRight");
+        motorBackLeft = hwMap.get(DcMotor.class, "motorBackLeft");
+        //TODO these might be forward, for their robot it was all reverse should be the same for all
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+        //TODO wtf is happening here
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //set power zero?
+        motorFrontLeft.setPower(0);
+        motorFrontRight.setPower(0);
+        motorBackLeft.setPower(0);
+        motorBackRight.setPower(0);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.loggingEnabled = true;
+        parameters.loggingTag     = "IMU";
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+    }
+    public void mecanumDrive(double Stick1Y, double Stick1X, double Stick2X, boolean A, boolean Y)  {
+
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        double motor0Raw = -Stick1Y - Stick1X - (Stick2X / 2);
+        double motor1Raw = Stick1Y - Stick1X - (Stick2X / 2);
+        double motor2Raw = Stick1Y + Stick1X - (Stick2X / 2);
+        double motor3Raw = -Stick1Y + Stick1X - (Stick2X / 2);
+
+        //Find max motor raw values //TODO fix in future
+        double rawMax = 0;//Math.max(Math.abs(motor0Raw) ,Math.max(Math.abs(motor1Raw) ,Math.max(Math.abs(motor2Raw) ,Math.max(Math.abs(motor3Raw)))));
+
+        //If any motor power value is outside -1 to 1, scale all values
+        if (rawMax > 1) {
+            motor0Scaled = motor0Raw / rawMax;
+            motor1Scaled = motor0Raw / rawMax;
+            motor2Scaled = motor0Raw / rawMax;
+            motor3Scaled = motor0Raw / rawMax;
+        } else {
+            motor0Scaled = motor0Raw;
+            motor1Scaled = motor0Raw;
+            motor2Scaled = motor0Raw;
+            motor3Scaled = motor0Raw;
+        }
+            //TODO 3:28 in video
+    }
+}
